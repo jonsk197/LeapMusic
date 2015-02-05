422bd919bf0530640b419b10920cf699aca6c2e3
jonsk197 <jonsk197@student.liu.se>
2015-02-05 14:17:03 +0100

    merge

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,22 +9,23 @@
 using namespace Leap;
 
 int main(int argc, char** argv) {
-	// Create a sample listener and controller
-	SampleListener listener;
-	Controller controller;
+  // Create a sample listener and controller
+    SampleListener listener;
+    Controller controller;
+    Menu menu;
 
-	// Have the sample listener receive events from the controller
-	controller.addListener(listener);
+  // Have the sample listener receive events from the controller
+  controller.addListener(listener);
 
-	if (argc > 1 && strcmp(argv[1], "--bg") == 0)
-		controller.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
+  if (argc > 1 && strcmp(argv[1], "--bg") == 0)
+    controller.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
 
-	// Keep this process running until Enter is pressed
-	std::cout << "Press Enter to quit..." << std::endl;
-	std::cin.get();
+  // Keep this process running until Enter is pressed
+  std::cout << "Press Enter to quit..." << std::endl;
+  std::cin.get();
 
-	// Remove the sample listener when done
-	controller.removeListener(listener);
+  // Remove the sample listener when done
+  controller.removeListener(listener);
 
-	return 0;
+  return 0;
 }
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -11,12 +11,12 @@ int menuSize = 50;
 
 void Menu::open(Vector startPosition){
     menuCenter = startPosition;
-    
+    updateMenu(startPosition);
 }
 
 void Menu::updateMenu(Vector handPosition){
 
-    if(abs(menuCenter - handPosition) > menuSize){
+    if(abs(Leap.vec3.distance(menuCenter, handPosition)) > menuSize){
     //do stuff when menuitem is selected
     }
 }
