ae4a398f720c00d2bbe5cb848f52153e6df15538
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-03-08 11:10:42 +0100

    Began implementing volume modulation on the x-axis.

--- src/ContinousSine.cpp
+++ src/ContinousSine.cpp
@@ -8,8 +8,13 @@
 #include "Sound.hpp"
 
 ContinousSine::ContinousSine(float frequency) :
-	playing(false), tableSize(100), position(0), lastF(1.f){
+	playing(false), volume(1.0), tableSize(100), position(0), lastF(0.01f){
 	setFrequency(frequency);
+}
+
+ContinousSine::ContinousSine(void) :
+	playing(false), volume(1.0), tableSize(100), position(0), lastF(0.01f){
+	setFrequency(Sound::A4);
 }
 
 int ContinousSine::PACallback(const void* inputBuffer,
@@ -25,7 +30,7 @@ int ContinousSine::PACallback(const void* inputBuffer,
 
 	if (playing) {
 		for (unsigned int i = 0; i < framesPerBuffer; i++){
-			float f = (float) sin(((float)position / tableSize)*M_PI*2.);
+			float f = (float) sin(((float)position / tableSize)*M_PI*2.0) * volume;
 
 			if (lastF > 0 && f < 0){
 				tableSize = nextTableSize;
@@ -52,4 +57,10 @@ void ContinousSine::setFrequency(double freq){
 
 double ContinousSine::getFrequency() {
 	return Sound::SAMPLE_RATE / nextTableSize;
+}
+
+
+void ContinousSine::setVolume(double vol) {
+	if (vol >= 0.0 && vol <= 1.0)
+		volume = vol;
 }
